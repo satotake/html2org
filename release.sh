6d795ccde2982884c463cb6295eaa32af9655233
satotake <doublequotation@gmail.com>
2021-05-07 02:44:40 +0900

    fix releaser

--- release.sh
+++ release.sh
@@ -5,8 +5,12 @@ git tag
 
 read -p "next version?: " tag
 
-sed -i "" "s/const version = \"v[[:digit:]]*.[[:digit:]]*.[[:digit:]]*\"/const version = \"$tag\"/" ./cmd/html2org/main.go
+cmd_go=./cmd/html2org/main.go
+sed -i "" "s/const version = \"v[[:digit:]]*.[[:digit:]]*.[[:digit:]]*\"/const version = \"$tag\"/" $cmd_go
+git add $cmd_go
+git commit -m "release $tag"
 
 git tag $tag
+git push origin $tag
 
 echo tag created $tag
